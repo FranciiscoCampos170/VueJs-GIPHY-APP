e860ec068da9346dfb4fe0b3fbfa92b47d19374d
Francisco Campos <franciiscocampos170@gmail.com>
2020-04-08 20:34:18 +0100

    add images to page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <h1>Ola VueJS!</h1>
+    <div v-for="data in datas" :key="data.id"> 
+      <div>
+        <img alt="" :src="data['images']['fixed_width']['url']">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +13,23 @@
 
 export default {
   name: 'App',
-
+  data(){
+    return {
+      datas: [],
+      ex: "ex"
+      }
+  },
+  mounted() {
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=s6avXyU73aCpWPNajq2vkLpiJpMHnb53&q='+"mrrobot", {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then((response) => response.json())
+        .then((res) => {this.datas = res.data}).catch((error) =>{ console.log(error)
+        }); 
+  }
+  
 }
 </script>
 
